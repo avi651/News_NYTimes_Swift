e4869bc95b730ae4b4eb6af3733fa8be7ae35fd9
Avinash Kumar <kumaravi651@gmail.com>
2024-01-15 18:29:02 +0530

    Initial Commit

---
+++ NyTimes/NyTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NyTimesApp.swift
+//  NyTimes
+//
+//  Created by Avinash on 15/01/24.
+//
+
+import SwiftUI
+
+@main
+struct NyTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
